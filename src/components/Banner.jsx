77ff23c8bb40b8b503c6949e099fa266cbd4ff82
makeda banza <makedabanza16@gmail.com>
2024-04-12 10:12:07 +0200

    up date header and banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,21 @@
 export default function Banner() {
-  return <div>Banner</div>;
+  return (
+    <>
+      <div className="relative xl:h-[90vh] bg-[#f3f1f5]">
+        <div className="w-full h-full flex flex-row p-10 justify-between items-center">
+          <img
+            src="src/assets/MakIMG-removebg-preview.png"
+            alt="Image 1"
+            className=" w-1/2 h-full object-contain rounded-6xl"
+          />
+          <div className="w-1/2 justify-center items-center space-y-4">
+            <p className="text-linkColor text-sm sm:text-3xl">Hello!</p>
+            <p className="text-textColor text-sm sm:text-3xl w-full xl:w-1/2 text-left xl:text-justify">
+              I'm Makeda Banza, Graduated in IT and Web and Mobile Developer.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,78 @@
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const links = [
+    {
+      name: "About me",
+      link: "",
+      className: "text-linkColor text-xl",
+    },
+    {
+      name: "Projects",
+      link: "",
+      className: "text-linkColor text-xl",
+    },
+    {
+      name: "Contact",
+      link: "tel:243825505783",
+      className: "text-linkColor text-xl",
+    },
+  ];
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
   return (
     <>
-      <div className="flex flex-row justify-between mx-10 bg-white my-2 items-center">
-        <h1 className="text-3xl font-bold text-textColor ">Makeda Banza</h1>
-        <div className="flex flex-row space-x-10 justify-between">
-          <p className=" text-linkColor">Mes réalisations</p>
-          <p className=" text-linkColor">Me contacter</p>
+      <div className="">
+        <div className="flex flex-row justify-between sm:mx-10 mx-5 py-2 bg-white my-2 items-center">
+          <Link to="" className="sm:text-3xl text-2xl font-bold text-textColor">
+            Makeda
+          </Link>
+          <ul className="hidden sm:flex  ">
+            {links.map((link) => (
+              <li key={link.name}>
+                <Link to={link.link} className={link.className}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div
+            onClick={() => setIsOpen(!isOpen)}
+            className="absolute right-4 top-3 cursor-pointer md:hidden"
+          >
+            {isOpen ? (
+              <Icon
+                className="text-linkColor w-10 h-10"
+                icon="material-symbols:close"
+              />
+            ) : (
+              <Icon
+                className="text-linkColor w-10 h-14"
+                icon="material-symbols-light:menu"
+              />
+            )}
+          </div>
+          <ul
+            className={` bg-white md:flex pl-5 md:pl-0  md:items-center md:pb-0 pb-12 z-[1] absolute left-0 w-full md:z-auto transition-all duration-300 ease-in flex md:flex-row flex-col justify-between 
+                    ${
+                      isOpen
+                        ? "top-14 rounded-b-2xl items-center"
+                        : "top-[-490px] rounded-b-2xl items-center"
+                    }`}
+          >
+            {links.map((link, index) => (
+              <li className={link.className} key={index}>
+                <Link to={link.link} onClick={closeMenu}>
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </>
